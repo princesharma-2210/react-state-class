32136a5c1e5e66559feb0bd4553c3a8d1fc371c2
princesharma-2210 <shaarmaprince@gmail.com>
2025-03-08 21:12:36 +0530

    add folder react@3-Todo-list and create a add function in todo list

---
+++ react@3-Todo-list/src/TodoList.jsx
@@ -0,0 +1,36 @@
+import {useState} from 'react'
+import { v4 as uuidv4 } from 'uuid'
+
+const TodoList = () => {
+    let [todos,setTodos]= useState([{task:"Sample Task", id:uuidv4()}]);
+    let [newTodo,setNewTodo]= useState("");
+
+    let newElement=()=>{
+        console.log("addding of task");
+        setTodos([...todos,{task:newTodo,id:uuidv4()}]);
+        setNewTodo("");
+    };
+    let updateTodoValue=(event)=>{
+        setNewTodo(event.target.value);
+    }
+  return (
+    <div>
+        <input placeholder='Add tasks' value={newTodo} onChange={updateTodoValue} ></input>
+        <button onClick={newElement}>ADD</button>
+        <br></br><br></br>
+        <hr></hr><br></br>
+        <h4>Tasks Todo</h4>
+        <br></br>
+        <ul>
+        {
+        todos.map((todo)=>(
+            <li key={todo.id}>{todo.task}</li>
+        ))
+    }
+        </ul>
+
+    </div>
+  )
+}
+
+export default TodoList
