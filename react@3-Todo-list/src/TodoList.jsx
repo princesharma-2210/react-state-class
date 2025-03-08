9bafecee62aef4619aa83df9844cd03a8f5cf580
princesharma-2210 <shaarmaprince@gmail.com>
2025-03-08 22:08:55 +0530

    perform delete operation in react@3-Todo-list folder

--- react@3-Todo-list/src/TodoList.jsx
+++ react@3-Todo-list/src/TodoList.jsx
@@ -5,18 +5,23 @@ const TodoList = () => {
     let [todos,setTodos]= useState([{task:"Sample Task", id:uuidv4()}]);
     let [newTodo,setNewTodo]= useState("");
 
-    let newElement=()=>{
+    let addNewElement=()=>{
         console.log("addding of task");
         setTodos([...todos,{task:newTodo,id:uuidv4()}]);
         setNewTodo("");
     };
     let updateTodoValue=(event)=>{
         setNewTodo(event.target.value);
-    }
+    };
+    let deleteElement=(id)=>{
+        console.log("delete tasks");
+        setTodos((prevTodo)=>todos.filter((prevTodo)=>prevTodo.id!== id));
+
+    };
   return (
     <div>
         <input placeholder='Add tasks' value={newTodo} onChange={updateTodoValue} ></input>
-        <button onClick={newElement}>ADD</button>
+        <button onClick={addNewElement}>ADD</button>
         <br></br><br></br>
         <hr></hr><br></br>
         <h4>Tasks Todo</h4>
@@ -24,13 +29,16 @@ const TodoList = () => {
         <ul>
         {
         todos.map((todo)=>(
-            <li key={todo.id}>{todo.task}</li>
+            <li key={todo.id}>
+                <span>{todo.task}</span>
+                &nbsp; &nbsp; &nbsp;
+                <button onClick={()=>{deleteElement(todo.id)}}>Delete</button>
+            </li>
         ))
     }
         </ul>
-
     </div>
-  )
+  );
 }
 
 export default TodoList
